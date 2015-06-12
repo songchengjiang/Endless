f2e9b20642c65387029f0f32072ce601403adf5a
songchengjiang <moses_jc@sina.com>
2015-06-12 18:09:46 +0800

    add files

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,236 @@
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
+
+    // add a "close" icon to exit the progress. it's an autorelease object
+    auto closeItem = MenuItemImage::create(
+                                           "CloseNormal.png",
+                                           "CloseSelected.png",
+                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+    
+	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
+                                origin.y + closeItem->getContentSize().height/2));
+
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(closeItem, NULL);
+    menu->setPosition(Vec2::ZERO);
+    this->addChild(menu, 1);
+
+    /////////////////////////////
+    // 3. add your codes below...
+
+    //// add a label shows "Hello World"
+    //// create and initialize a label
+    //
+    //auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    //
+    //// position the label on the center of the screen
+    //label->setPosition(Vec2(origin.x + visibleSize.width/2,
+    //                        origin.y + visibleSize.height - label->getContentSize().height));
+
+    //// add the label as a child to this layer
+    //this->addChild(label, 1);
+
+    //// add "HelloWorld" splash screen"
+    //auto sprite = Sprite::create("HelloWorld.png");
+
+    //// position the sprite on the center of the screen
+    //sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+
+    //// add the sprite as a child to this layer
+    //this->addChild(sprite, 0);
+
+	_center = Vec2(visibleSize.width / 2.0f, visibleSize.height / 2.0f);
+	auto listener = EventListenerTouchAllAtOnce::create();
+	listener->onTouchesBegan = CC_CALLBACK_2(HelloWorld::onTouchBegining, this);
+	listener->onTouchesMoved = CC_CALLBACK_2(HelloWorld::onTouchMoving, this);
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+
+	_renderTex = RenderTexture::create(visibleSize.width, visibleSize.height, Texture2D::PixelFormat::RGBA8888);
+	_renderTex->setPosition(_center);
+	_renderTex->retain();
+	_renderTex->clear(1.0f, 1.0f, 1.0f, 1.0f);
+	this->addChild(_renderTex, -1);
+
+	_drawNode = DrawNode::create();
+	_drawNode->setPosition(_center);
+	_drawNode->drawCircle(Vec2(300.0f - 140.0f, 0.0f), 140.0f, 0.0, 32, false, Color4F(0.5f, 0.5f, 0.5f, 1.0f));
+	this->addChild(_drawNode);
+
+	drawRangeLine(300.0f);
+	scheduleUpdate();
+
+	_theta = 0.0f;
+	_baseMat.translate(Vec3(visibleSize.width / 2.0f, visibleSize.height / 2.0f, 0.0f));
+	_firstMat.translate(Vec3(300.0f - 140.0f, 0.0f, 0.0f));
+	_secondMat.translate(Vec3(0.0f, 0.0f, 0.0f));
+    return true;
+}
+
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    return;
+#endif
+
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
+
+void HelloWorld::drawLines()
+{
+	{
+		float patternCircleR = 140.0f;
+		float circleR = 300.0f;
+		float markR = 100.0f;
+		drawPattern(circleR, patternCircleR, markR, Color3B(152, 235, 207));
+	}
+
+	{
+		float patternCircleR = 140.0f;
+		float circleR = 300.0f;
+		float markR = 80.0f;
+		drawPattern(circleR, patternCircleR, markR, Color3B(102, 180, 203));
+	}
+
+
+	{
+		float patternCircleR = 140.0f;
+		float circleR = 300.0f;
+		float markR = 60.0f;
+		drawPattern(circleR, patternCircleR, markR, Color3B(232, 229, 210));
+	}
+
+
+	{
+		float patternCircleR = 140.0f;
+		float circleR = 300.0f;
+		float markR = 40.0f;
+		drawPattern(circleR, patternCircleR, markR, Color3B(207, 110, 93));
+	}
+}
+
+void HelloWorld::drawPattern(float radius1, float radius2, float radius3, const cocos2d::Color3B &color)
+{
+	_renderTex->begin();
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	_baseMat = Mat4::IDENTITY;
+	_firstMat = Mat4::IDENTITY;
+	_secondMat = Mat4::IDENTITY;
+
+	_baseMat.translate(Vec3(visibleSize.width / 2.0f, visibleSize.height / 2.0f, 0.0f));
+	_firstMat.translate(Vec3(radius1 - radius2, 0.0f, 0.0f));
+	_secondMat.translate(Vec3(radius3, 0.0f, 0.0f));
+
+	_firstMat.rotate(Vec3(0.0f, 0.0f, 1.0f), -_theta);
+	float theta1 = (_theta * radius2) / radius1;
+	_baseMat.rotate(Vec3(0.0f, 0.0f, 1.0f), theta1);
+
+	Mat4 mat = _baseMat * _firstMat * _secondMat;
+	auto sprite = Sprite::create("fire.png");
+	sprite->setScale(0.2f);
+	sprite->setColor(color);
+	sprite->setOpacity(128);
+	sprite->setPosition(mat.m[12], mat.m[13]);
+	sprite->visit();
+	_renderTex->end();
+}
+
+void HelloWorld::update(float delta)
+{
+	//if (_theta < 2 * M_PI * 50){
+	//	//for (unsigned int i = 0; i < 10; ++i){
+	//		drawLines();
+	//		_theta += 0.01;
+	//	//}
+	//}
+	//else{
+	//	_renderTex->saveToFile("tex.png");
+	//	unscheduleUpdate();
+	//}
+}
+
+void HelloWorld::drawRangeLine(float radius)
+{
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	_renderTex->begin();
+	for (float theta = 0.0f; theta < 2.0f * M_PI; theta += 0.01f){
+		float x = radius * cosf(theta);
+		float y = radius * sinf(theta);
+		auto sprite = Sprite::create("fire.png");
+		sprite->setScale(0.1f);
+		sprite->setColor(Color3B::BLACK);
+		sprite->setPosition(x + visibleSize.width / 2.0f, y + visibleSize.height / 2.0f);
+		sprite->visit();
+	}
+	_renderTex->end();
+}
+
+void HelloWorld::onTouchMoving(const std::vector<cocos2d::Touch*> &touchs, cocos2d::Event *event)
+{
+	if (0 < touchs.size()){
+		auto location = touchs[0]->getLocation();
+		auto preLocation = touchs[0]->getPreviousLocation();
+		Vec2 dir = location - _center;
+		Vec2 preDir = preLocation - _center;
+		dir.normalize();
+		preDir.normalize();
+
+		float theta = acosf(dir.dot(preDir));
+		theta = dir.y < preDir.y ? -theta : theta;
+		_theta += theta;
+		drawPattern(300.0f, 140.0f, _markRadius, Color3B(152, 235, 207));
+	}
+}
+
+void HelloWorld::onTouchBegining(const std::vector<cocos2d::Touch*> &touchs, cocos2d::Event *event)
+{
+	if (0 < touchs.size()){
+		auto location = touchs[0]->getLocation();
+		Mat4 inv = (_baseMat * _firstMat).getInversed();
+		Vec3 localPos;
+		inv.transformPoint(Vec3(location.x, location.y, 0.0f), &localPos);
+		_markRadius = localPos.length();
+	}
+}
+
+void HelloWorld::onTouchEnding(const std::vector<cocos2d::Touch*> &touchs, cocos2d::Event *event)
+{
+}
---
+++ Classes/HelloWorldScene.h
@@ -0,0 +1,46 @@
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+
+class HelloWorld : public cocos2d::Layer
+{
+public:
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene();
+
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();
+    
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(HelloWorld);
+
+	virtual void update(float delta) override;
+
+private:
+
+	void drawLines();
+	void drawPattern(float radius1, float radius2, float radius3, const cocos2d::Color3B &color);
+	void drawRangeLine(float radius);
+	void onTouchBegining(const std::vector<cocos2d::Touch*> &touchs, cocos2d::Event *event);
+	void onTouchMoving(const std::vector<cocos2d::Touch*> &touchs, cocos2d::Event *event);
+	void onTouchEnding(const std::vector<cocos2d::Touch*> &touchs, cocos2d::Event *event);
+
+private:
+
+	cocos2d::RenderTexture *_renderTex;
+	cocos2d::DrawNode *_drawNode;
+	float _theta;
+	cocos2d::Vec3 _currentCirclePos;
+	cocos2d::Vec2 _center;
+	float _markRadius;
+
+	cocos2d::Mat4 _baseMat;
+	cocos2d::Mat4 _firstMat;
+	cocos2d::Mat4 _secondMat;
+};
+
+#endif // __HELLOWORLD_SCENE_H__
